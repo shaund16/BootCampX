8dab4dccc7d2f6d290e91ce03b554c45a098e5fe
Shaun <s-purslow@hotmail.com>
2021-07-21 18:59:22 +0000

    modifiy the teachers file

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,20 +7,24 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2] || 'JUL02'}%' 
-ORDER BY teacher;
-`)
+const queryString = `
+SELECT DISTINCT(teachers.name) AS teacher, cohorts.name AS cohort
+    FROM teachers
+    JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
+    JOIN students ON students.id = assistance_requests.student_id
+    JOIN cohorts ON cohorts.id = students.cohort_id
+    WHERE cohorts.name = $1
+    ORDER BY teacher;
+`;
+
+const cohortName = process.argv[2] || 'JUL02';
+
+pool.query(queryString, [cohortName])
   .then(res => {
-    res.rows.forEach(row => {
-      console.log(`${row.cohort}: ${row.teacher}`);
+    res.rows.forEach(teacher => {
+      console.log(`${teacher.cohort}: ${teacher.teacher}`);
     });
-  });
+  }).catch(err => console.error('query error', err.stack));
 
 
   
